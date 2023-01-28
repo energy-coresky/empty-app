e2402d0e31da37141223fe1dbb676145d66fefdd
Energy <71418770+energy-coresky@users.noreply.github.com>
2023-01-28 20:08:49 +0200

    tiny fix

--- main/conf.php
+++ main/conf.php
@@ -9,13 +9,11 @@ define('PHP_TZ', 'Europe/Kiev');
 define('DEFAULT_LG', '');
 define('EXTRA', 0);     // 0 - off, 1 - adjust mode, 2 - run (don't check links list)
 define('CACHE', 0);
-define('ENC', 'UTF-8');
 
 ini_set('log_errors', 0);     # 0
 ini_set('display_errors', 1); # 1
 ini_set('error_reporting', DEBUG ? -1 : 0);
 
-define('CLI', 'cli' == PHP_SAPI);
 define('DIR_M', basename(__DIR__));
 define('DIR_S', is_dir('../air') ? '../air' : 'vendor/energy/air');
 
@@ -24,4 +22,3 @@ require DIR_S . '/heaven.php';
 
 #SKY::$databases = ['driver' => 'mysqli', 'dsn' => 's localhost root '];
 SKY::$databases = ['driver' => 'sqlite3', 'dsn' => 'hole.base'];
-
