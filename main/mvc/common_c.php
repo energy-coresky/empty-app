fe9fdbdca23c378d2e09812923950fb29f1dec79
Energy <71418770+energy-coresky@users.noreply.github.com>
2023-02-09 18:34:14 +0200

    tiny fix

--- main/mvc/common_c.php
+++ main/mvc/common_c.php
@@ -32,7 +32,6 @@ class common_c extends Controller
 
        $sky->ajax or MVC::$layout = $sky->is_mobile ? 'mobile' : 'desktop';
         $sky->show_pdaxt = true;
-        define('LINK', PROTO . '://' . DOMAIN . PORT . PATH);
 
         $no_auth = $sky->_0 == '_init' || $sky->_0 == 'main' && $sky->_1 == 'lang';
 
