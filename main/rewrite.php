130b18cfedd7fd818343c93f8b8ed8c252276b5b
Energy <71418770+energy-coresky@users.noreply.github.com>
2023-04-08 22:08:46 +0300

    Tune rewrites

--- main/rewrite.php
+++ main/rewrite.php
@@ -5,43 +5,27 @@
 return array (
   0 => 
   array (
-    0 => 'Standard',
-    1 => 'global $sky;
-$_0 = $cnt ? $surl[0] : \'\';',
+    0 => 'Main-page',
+    1 => '$main = \'\' === $uri;
+if ($main || \'main\' === $uri)
+    return $surl = $main ? [\'main\'] : [];',
     2 => '0',
-    3 => '/a /a',
+    3 => '/',
   ),
   1 => 
   array (
-    0 => 'Assets',
-    1 => 'if (\'m\' == $_0)
-    return $surl[0] = \'etc\';',
-    2 => '1',
-    3 => '/m/sky.js /etc/sky.js',
+    0 => 'Robots.txt',
+    1 => 'if (\'robots.txt\' == $uri)
+    return array_unshift($surl, \'etc\');',
+    2 => '0',
+    3 => '/robots.txt',
   ),
   2 => 
   array (
-    0 => 'Dev-tools',
-    1 => 'if ($sky->_0 && \'_\' == $sky->_0[0])
-    return;',
+    0 => 'Assets',
+    1 => 'if ($cnt && \'m\' == $surl[0])
+    return $surl[0] = \'etc\';',
     2 => '1',
-    3 => '/a /a',
-  ),
-  3 => 
-  array (
-    0 => 'Main-page',
-    1 => '$main = \'\' === URI;
-if ($main || \'main\' === URI)
-    return $surl = $main ? [\'main\'] : [];',
-    2 => '0',
-    3 => '/ /main',
-  ),
-  4 => 
-  array (
-    0 => 'Robots.txt',
-    1 => 'if (\'robots.txt\' == URI)
-    return array_unshift($surl, \'etc\');',
-    2 => '0',
-    3 => '/robots.txt /etc/robots.txt',
+    3 => '/m/sky.js',
   ),
 );
