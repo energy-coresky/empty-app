f996d0b8f7c74b058a204f5dc99270d819af8442
Energy <71418770+energy-coresky@users.noreply.github.com>
2024-12-15 18:35:18 +0300

    tiny fix

--- main/tests/nice.php
+++ main/tests/nice.php
@@ -2,41 +2,21 @@
 
 (function() use ($m) {
     $list = glob(DIR_S . '/w2/*.php') + [
-        -1 => DIR_S . '/sky.php',
-        -2 => DIR_S . '/sky',
+        -1 => DIR_S . '/sky.php', -2 => DIR_S . '/sky',
         -3 => 'xsp.php',
     ];
+    //$list = ['main/tests/zz_test.php'];
     $list = array_combine($list, array_map(fn($v) => basename($v), $list));
     $list = array_unique($list);
-    $fp = fopen(__FILE__ . '.test', 'w');
+    $fp = fopen(__FILE__ . '.diff', 'w');
+    PHP::$autocomma = false;
+    
     foreach ($list as $path => $fn) {
         $old = is_file($_fn = "main/tests/files/$fn") ? file_get_contents($_fn) : "\n\n\n";
-        $php = PHP::file($path);
-        file_put_contents("$_fn$m", $new = (string)$php);
-       $diff = '?';
-        try {
-            $rN = Display::diff($new, $old);
-            if ($diff = strlen($rN) - substr_count($rN, '=')) {
-                fputs($fp, "$fn DIFF = $diff\n");
-                $new = explode("\n", unl($new));
-                $old = explode("\n", unl($old));
-                $on = $ol = '';
-                if ($diff < 9) for ($i = 0, $n = $l = 1, $cnt = strlen($rN); $i < $cnt; $i++) {
-                    $d = $rN[$i];
-                    if ('=' != $d) {
-                        $on .= "  $n | " . ('.' != $d ? $new[$n - 1] : '') . "\n";
-                        $ol .= "  $l | " . ('+' != $d ? $old[$l - 1] : '') . "\n";
-                    }
-                    '.' == $d or $n++;
-                    '+' == $d or $l++;
-                }
-                if ($on)
-                    fputs($fp, " new =====\n$on old =====\n$ol");
-            } else {
-                $diff = '.';
-            }
-        } catch (throwable $e) {}
-        
-        echo " $diff $fn\n";
+        file_put_contents("$_fn$m", $new = (string)PHP::file($path));
+        [$out, $add, $sub] = Display::diffx($new, $old);
+        $diff = ($sub ? "-$sub" : '') . ($add ? "+$add" : '');
+        $out && fputs($fp, sprintf("@@ %s @@ $fn\n", $diff) . ($add + $sub < 9 ? $out : ''));
+        echo $out ? " $diff $fn\n" : " . $fn\n";
     }
 })();
